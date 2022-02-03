e8c47fb94ced183dbdc3c6a967e642e93eb93344
mjjrt <64268845+mjjrt@users.noreply.github.com>
2022-02-03 22:21:20 +0100

    03Feb2022

--- Assist.cpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-double GetRandomStep()
-{
-    std::random_device rseed;
-    std::mt19937 rng(rseed());
-    std::uniform_int_distribution<int> dist(-10,10);
-    return dist(rng);
-}
